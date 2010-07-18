fb79f94a1218f702eecb8519cc522af945595a50
Spencer Tipping <spencer@spencertipping.com>
2010-07-17 22:26:13 -0600

    Last commit before thinking about what I'm doing. Using vtable lookups is probably too structured; hooking into that using the JIT is going to be a lot of work. Better is probably to reduce to a typeclass pattern and use structs whose first member is an aligned function pointer.

---
+++ src/bootstrap/reader/parser.hh
@@ -0,0 +1,36 @@
+#ifndef BOOTSTRAP_READER_PARSER_HH
+#define BOOTSTRAP_READER_PARSER_HH
+
+#include <string>
+#include <istream>
+
+#include "bootstrap/core/definitions.hh"
+
+BEGIN_NAMESPACE()
+
+namespace internal {
+
+using namespace std;
+
+static gnarly_ref read (istream &in) {
+  char       c;
+  gnarly_ref value;
+
+  
+
+  while (in >> c && c != ')')
+}
+
+}
+
+class $asterisk$lt$lt : public gnarly_value {
+public:
+  $asterisk$lt$lt () {}
+  virtual ~$asterisk$lt$lt () {}
+
+  
+};
+
+END_NAMESPACE()
+
+#endif
